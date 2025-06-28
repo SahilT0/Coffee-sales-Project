59b3910c5678785cb8d1d9f046e64325b3401916
SahilT0 <Sahiltuli16117@gmail.com>
2025-06-28 10:15:36 +0530

    Cleaned data by handling missing values, adding time features, and generating customer frequency for modeling

--- Pyhton.py
+++ Pyhton.py
@@ -31,3 +31,45 @@ print(sales.describe())
 # Converting Datatype
 sales['date'] = pd.to_datetime(sales['date'])
 sales['datetime'] = pd.to_datetime(sales['datetime'])
+
+# Missing values
+print(sales.isnull().sum())
+# card has 89 missing value which is of the cash user
+
+# Handle missing value
+sales['card'] = sales['card'].fillna("Cash_User")
+
+print(sales.head(10))
+
+# Adding column Month, Day, Hour
+sales['month'] = sales['date'].dt.strftime('%Y-%m')
+sales['day'] = sales['date'].dt.day_name()
+sales['hours'] = sales['datetime'].dt.hour
+
+print(sales.head(15))
+print(sales.info())
+
+# Card value count
+card_count = sales['card'].value_counts()
+print(card_count)
+
+# New column Customer_Frequency
+sales['customer_frequency'] = sales['card'].map(card_count)
+
+print(sales.head(25))
+# cash user showing the frequency of 89 , now making them as 0
+filtered_sales = sales[sales['card'] != 'Cash_User']
+card_count = filtered_sales['card'].value_counts()
+sales['customer_frequency'] = sales['card'].map(card_count)
+sales['customer_frequency'] = sales['customer_frequency'].fillna(0).astype(int)
+
+# Drop unneeded columns for ML modeling
+cleaned_data = sales.drop(columns=['date', 'datetime', 'card'])
+print(cleaned_data.head())
+
+# Save Cleaned data to new CSV
+cleaned_data.to_csv('C:/Users/SAHIL/Unified Mentor/Project (Data Analyst)/Coffee sales/cleaned_coffee_sales.csv', index=False)
+
+# Data Cleaning Done
+
+
